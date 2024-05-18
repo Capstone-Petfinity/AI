37c0ef7c6583fe80de62c513a67e87fd9c3b169b
me11ong <121924879+me11ong@users.noreply.github.com>
2024-05-19 01:12:18 +0900

    test.py 포트 업데이트

--- test.py
+++ test.py
@@ -1,15 +1,15 @@
 from flask import Flask, render_template
 from PIL import Image
 import os
+from flask import Flask
 
 app = Flask(__name__)
 
-
-@app.route("/hello")
+@app.route('/hello')
 def hello():
+    return 'Hello, World!'
 
-    return "hello world!"
+if __name__ == '__main__':
+    # Flask 애플리케이션을 0.0.0.0 호스트와 20002 포트로 실행
+    app.run(host='0.0.0.0', port=20002)
 
-
-if __name__ == "__main__":
-    app.run()
