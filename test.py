27ced26e5267aaf8e4e488021cea0a8959cd6ba8
me11ong <121924879+me11ong@users.noreply.github.com>
2024-05-20 00:09:59 +0900

    test.py 살림

---
+++ test.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/hello')
+def hello():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    # Flask 애플리케이션을 0.0.0.0 호스트와 20002 포트로 실행
+    app.run(host='0.0.0.0', port=20002)
